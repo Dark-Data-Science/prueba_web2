a543d761bdccba26670a7a564a06f058217681d5
Dark-Data-Science <j7416284@gmail.com>
2024-03-30 17:11:30 -0300

    consolidado

--- django_portfolio/portfolio/models.py
+++ django_portfolio/portfolio/models.py
@@ -7,5 +7,4 @@ class Project(models.Model):
     title = models.CharField(max_length=100)  
     description = models.CharField(max_length=250)  
     image = models.ImageField(upload_to='portfolio/images/') 
-    url = models.URLField(blank=True)
     date = DateField(default=date.today) 
